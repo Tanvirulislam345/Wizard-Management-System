3f6efd7995c3dd69bb773e99f6ec7b89cda1019c
Tanvirulislam345 <tanvir.topader345@gmail.com>
2022-04-20 17:08:04 +0600

    invoice

--- wizard-project/src/components/layout/SideBar.js
+++ wizard-project/src/components/layout/SideBar.js
@@ -1,110 +1,110 @@
-import { Box, Drawer, useMediaQuery } from '@mui/material';
-import HouseIcon from '@mui/icons-material/House';
-import { NavItem } from './NavItem';
-import { useStyle } from '../../styles/MetarialStyles';
-
-
+import { Box, Drawer, useMediaQuery } from "@mui/material";
+import { NavItem } from "./NavItem";
+import { useStyle } from "../../styles/MetarialStyles";
+import {
+  FaHome,
+  FaProjectDiagram,
+  FaDonate,
+  FaUserSecret,
+  FaUsers,
+  FaDollyFlatbed,
+} from "react-icons/fa";
 
 const items = [
   {
     href: "/",
-    icon: <HouseIcon fontSize="small" />,
+    icon: <FaHome fontSize="small" />,
     title: "Home",
   },
   {
     href: "/project",
-    icon: <HouseIcon fontSize="small" />,
+    icon: <FaProjectDiagram fontSize="small" />,
     title: "Project",
   },
   {
     href: "/employee",
-    icon: <HouseIcon fontSize="small" />,
+    icon: <FaUsers fontSize="small" />,
     title: "Employee",
   },
   {
     href: "/client",
-    icon: <HouseIcon fontSize="small" />,
+    icon: <FaUserSecret fontSize="small" />,
     title: "Client",
   },
   {
     href: "/payment",
-    icon: <HouseIcon fontSize="small" />,
+    icon: <FaDollyFlatbed fontSize="small" />,
     title: "Payment",
   },
   {
     href: "/expense",
-    icon: <HouseIcon fontSize="small" />,
+    icon: <FaDonate fontSize="small" />,
     title: "Expense",
   },
 ];
 
 export default function SideBar({ open, onClose }) {
-    const style = useStyle();
-    const lgUp = useMediaQuery((theme) => theme.breakpoints.up('lg'), {
-        defaultMatches: true,
-        noSsr: false
-    });
+  const style = useStyle();
+  const lgUp = useMediaQuery((theme) => theme.breakpoints.up("lg"), {
+    defaultMatches: true,
+    noSsr: false,
+  });
 
-
-    const content = (
-        <>
-            <Box
-                className={style.sidebar}
-            >
-
-                <Box sx={{
-                    flexGrow: 1
-                }}>
-                    {items.map((item) => (
-                        <NavItem
-                            key={item.title}
-                            icon={item.icon}
-                            href={item.href}
-                            title={item.title}
-                            onClose={onClose}
-                        />
-                    ))}
-                </Box>
-                {/* <Divider sx={{ borderColor: '#2D3748' }} /> */}
-
-            </Box>
-        </>
-    );
-
-    if (lgUp) {
-        return (
-            <Drawer
-                variant="permanent"
-                anchor="left"
-                open
-                PaperProps={{
-                    sx: {
-                        width: 280,
-                        border: 'none',
-                    }
-                }}
-            >
-                {content}
-            </Drawer>
-
-        );
-    }
-
-    return (
-        <Drawer
-            variant="temporary"
-            anchor="left"
-            onClose={onClose}
-            open={open}
-            PaperProps={{
-                sx: {
-                    width: 280,
-                    border: 'none',
-                }
-            }}
+  const content = (
+    <>
+      <Box className={style.sidebar}>
+        <Box
+          sx={{
+            flexGrow: 1,
+          }}
         >
-            {content}
-        </Drawer>
+          {items.map((item) => (
+            <NavItem
+              key={item.title}
+              icon={item.icon}
+              href={item.href}
+              title={item.title}
+              onClose={onClose}
+            />
+          ))}
+        </Box>
+        {/* <Divider sx={{ borderColor: '#2D3748' }} /> */}
+      </Box>
+    </>
+  );
 
+  if (lgUp) {
+    return (
+      <Drawer
+        variant="permanent"
+        anchor="left"
+        open
+        PaperProps={{
+          sx: {
+            width: 280,
+            border: "none",
+          },
+        }}
+      >
+        {content}
+      </Drawer>
     );
-};
+  }
+
+  return (
+    <Drawer
+      variant="temporary"
+      anchor="left"
+      onClose={onClose}
+      open={open}
+      PaperProps={{
+        sx: {
+          width: 280,
+          border: "none",
+        },
+      }}
+    >
+      {content}
+    </Drawer>
+  );
+}
--- wizard-project/src/pages/Invoice.js
+++ wizard-project/src/pages/Invoice.js
@@ -1,15 +1,11 @@
 import React from "react";
 import SubNav2 from "../components/subNav/SubNav2";
 import { LayoutContiner } from "../styles/MetarialStyles";
-import InvoicePrintDawnload from "../components/shared/Invoice/InvoicePrintDawnload";
-import InvoicePaper from "../components/shared/Invoice/InvoicePaper";
 
 const Invoice = () => {
   return (
     <LayoutContiner>
       <SubNav2 project="Invoice" />
-      <InvoicePrintDawnload></InvoicePrintDawnload>
-      <InvoicePaper></InvoicePaper>
     </LayoutContiner>
   );
 };
